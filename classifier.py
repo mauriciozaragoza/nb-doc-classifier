d98d0745beeaa64acfd34f5db61344f160d406b0
Mauricio Zaragoza <kingk912@gmail.com>
2014-03-13 22:01:22 -0600

    added plotting functions

--- classifier.py
+++ classifier.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 from math import log
 
@@ -9,12 +10,11 @@ cy = np.zeros(ny)
 py = np.empty(ny)
 
 alpha = 0.01
-doc_label = np.zeros(nx)
+doc_label = np.zeros(nd)
 test_label = []
 confusion = np.zeros((ny,ny), dtype = np.int32)
 
 bag = np.zeros((ny, nx), dtype = np.float64)
-bag[:] = alpha
 
 def read_counts(filename):
 	global py
@@ -28,7 +28,7 @@ def read_counts(filename):
 
 	py = cy / float(nd)
 
-def read_validation(filename):
+def read_validation(filename, bag):
 	current_id = 0
 
 	sample = np.empty(nx) # [0 for i in range(nx)]
@@ -43,7 +43,7 @@ def read_validation(filename):
 		word_count = int(params[2].strip())
 
 		if (doc_id != current_id):	
-			label = classify(sample)
+			label = classify(sample, bag)
 			if label == test_label[current_id]:
 				correct += 1
 
@@ -60,13 +60,14 @@ def read_validation(filename):
 	
 	confusion[test_label[current_id]][label] += 1
 
-	if classify(sample) == test_label[current_id]:
+	if classify(sample, bag) == test_label[current_id]:
 		correct += 1
 
 	print("correctly classified documents: " + str(correct))
 	print("total number of tested documents: " + str(current_id + 1))
 
 	print("accuracy: " + str(correct / float(current_id + 1)))
+	return correct / float(current_id + 1)
 
 def read_validation_label(filename):
 	for line in open(filename, "r"):
@@ -83,22 +84,22 @@ def read_bag(filename):
 
 		bag[label][word_id] += word_count
 
-def map_estimate():
-	global bag
-
-	total_words = bag.sum(1)
+def map_estimate(bag):
+	posteriori = np.copy(bag)
+	posteriori += alpha
+	total_words = posteriori.sum(1)
 	
 	regularization = total_words + alpha
-	for i in range(len(bag[0])):
-		bag[:, i] /= regularization
+	for i in range(len(posteriori[0])):
+		posteriori[:, i] /= regularization
 
-	map_probabilities = bag.sum(1);
+	map_probabilities = posteriori.sum(1);
 
 	assert abs((map_probabilities).sum() - ny) < 0.01, "conditional probabilities do not sum to 1, actual probability: " + str(map_probabilities) + " , " + str((map_probabilities).sum())
 
-	bag = np.log2(bag)
+	return np.log2(posteriori)
 
-def classify(d):
+def classify(d, bag):
 	probabilities = (np.dot(bag, d) + py)
 	# print(probabilities)
 	return probabilities.argmax()
@@ -109,16 +110,33 @@ def print_confusion():
 
 
 # MAIN
+n_samples = 20
+# xaxis = sorted(np.logspace(log(0.00001, 10), log(1, 10), num = n_samples, base = 10))
+xaxis = sorted(np.linspace(0.00005, 0.0005, num = 10))
+
+print("testing on alphas: " + str(xaxis))
+yaxis = []
+
+plt.xlabel('Alpha')
+plt.ylabel('Accuracy')
+# plt.xscale('log')
+plt.grid(True)
 
 print("reading counts")
 read_counts("data/train.label")
 print("reading bag")
 read_bag("data/train.data")
-print("computing MAP parameters")
-map_estimate()
 print("reading labels")
 read_validation_label("data/test.label")
-print("classifying")
-read_validation("data/test.data")
 
+for a in xaxis:
+	alpha = a
+	print("computing MAP parameters")
+	posteriori = map_estimate(bag)
+	print("classifying")
+	yaxis.append(read_validation("data/test.data", posteriori))
+
+print ("results: " + str(yaxis))
+plt.plot(xaxis, yaxis)
+plt.show();
 #print_confusion()
